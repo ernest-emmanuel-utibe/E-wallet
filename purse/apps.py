f978fc0ed6ab4b2fb1e148946f2bb202bbca648e
Ernest <em123nest@gmail.com>
2023-04-13 01:01:59 +0100

    wallet commit

---
+++ purse/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PurseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'purse'
---
+++ purse/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(unique=True, max_length=11)
+
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+
